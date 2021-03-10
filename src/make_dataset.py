0af915b98a7459b2d98b36810e010a6224a96418
Yoichi Ishibashi <yoichi1484@gmail.com>
2021-03-11 02:41:01 +0900

    Update make_dataset.py

--- src/make_dataset.py
+++ src/make_dataset.py
@@ -2,7 +2,58 @@ import os
 import argparse
 import json
 import numpy as np
-import neuplanet
+
+from  glob import glob
+from tqdm import tqdm
+import numpy as np
+import starry
+
+np.random.seed(12)
+starry.config.lazy = False
+starry.config.quiet = True
+
+
+def make_dataset(path_dataset, n_data, ydeg, amp, obl, inc, npts, nrot):
+    path_dataset
+    files = sorted(glob("{}/*.png".format(path_dataset)))
+    if n_data > 0:
+      files = files[:n_data]
+    light_curves = []
+    for filename in tqdm(files):
+        flux, _ = get_light_curve(filename, ydeg, amp, obl, inc, npts, nrot)
+        light_curves.append(flux)
+    return np.array(light_curves)
+  
+def get_light_curve(path_img, ydeg, amp, obl, inc, npts, nrot, sigma=0.005):
+    # Initialize map
+    map = starry.Map(ydeg=ydeg, reflected=True)
+    map.load(path_img)
+    map.amp = amp
+    map.obl = obl
+    map.inc = inc
+
+    # Make the planet rotate 10 times over one full orbit
+    time = np.linspace(0, 1, npts)
+    theta = np.linspace(0, 360 * nrot, npts)
+
+    # Position of the star relative to the planet in the orbital plane
+    t = np.reshape(time, (1, -1))
+    p = np.vstack((np.cos(2 * np.pi * t), np.sin(2 * np.pi * t), 0 * t))
+
+    # Rotate to an observer inclination of 60 degrees
+    ci = np.cos(map.inc * np.pi / 180)
+    si = np.sin(map.inc * np.pi / 180)
+    R = np.array([[1, 0, 0], [0, ci, -si], [0, si, ci]])
+    xs, ys, zs = R.dot(p)
+
+    # Keywords to the `flux` method
+    kwargs = dict(theta=theta, xs=xs, ys=ys, zs=zs)
+
+    # Compute the flux
+    flux0 = map.flux(**kwargs)
+    flux = flux0 + sigma * np.random.randn(npts)
+
+    return flux, map
 
 parser = argparse.ArgumentParser(description='A script of making NeuPlaNet dataset')
 parser.add_argument('--path_img', type=str, default="n10000_64x32_bin")
@@ -24,8 +75,8 @@ else:
   n_data = -1 # use all images
   
 print("computing flux")
-fluxes = neuplanet.make_dataset(args.path_img, n_data, args.ydeg, args.amp, 
-                                args.obl, args.inc, args.npts, args.nrot)
+fluxes = make_dataset(args.path_img, n_data, args.ydeg, args.amp, 
+                      args.obl, args.inc, args.npts, args.nrot)
 
 print("saving...")
 os.makedirs(args.path_save, exist_ok=True)
