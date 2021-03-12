c1250810db09f0d21b81e3dd765a2e5fed31b520
Yoichi Ishibashi <yoichi1484@gmail.com>
2021-03-12 20:18:48 +0900

    Update make_dataset.py

--- src/make_dataset.py
+++ src/make_dataset.py
@@ -16,6 +16,7 @@ def make_dataset(path_dataset, n_data, ydeg, amp, obl, inc, npts, nrot, fluxes=[
         files = files[idx:idx+n_data]
     else:
         files = files[idx:]
+    print(files[idx])
     print("computing flux from [{}] (index: {})".format(files[idx], idx))
     
     assert len(files) != 0
