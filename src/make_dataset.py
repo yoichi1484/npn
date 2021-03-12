92a067d3d192b30c209d4708ae9227244ce7f1f9
Yoichi Ishibashi <yoichi1484@gmail.com>
2021-03-12 17:23:23 +0900

    Update make_dataset.py

--- src/make_dataset.py
+++ src/make_dataset.py
@@ -1,53 +1,68 @@
-import os
-import argparse
-import json
 import numpy as np
+import torch
+import starry
 
-from  glob import glob
-from tqdm import tqdm
-import numpy as np
-import utils
+np.random.seed(12)
+starry.config.lazy = False
+starry.config.quiet = True
 
 
-def make_dataset(path_dataset, n_data, ydeg, amp, obl, inc, npts, nrot):
-    path_dataset
-    files = sorted(glob("{}/*.png".format(path_dataset)))
-    if n_data > 0:
-      files = files[:n_data]
-    assert len(files) != 0
-    light_curves = []
-    for filename in tqdm(files):
-        flux, _ = utils.get_light_curve(filename, ydeg, amp, obl, inc, npts, nrot)
-        light_curves.append(flux)
-    return np.array(light_curves)
+def get_light_curve(path_img, ydeg, amp, obl, inc, npts, nrot, sigma=0.005):
+    # Initialize map
+    map = starry.Map(ydeg=ydeg, reflected=True)
+    map.load(path_img)
+    map.amp = amp
+    map.obl = obl
+    map.inc = inc
 
-parser = argparse.ArgumentParser(description='A script of making NeuPlaNet dataset')
-parser.add_argument('--path_img', type=str, default="n10000_64x32_bin")
-parser.add_argument('--path_save', type=str, default="flux")
-parser.add_argument('--ydeg', type=int, default=10)
-parser.add_argument('--amp', type=int, default=1.3)
-parser.add_argument('--obl', type=int, default=23.5)
-parser.add_argument('--inc', type=int, default=60)
-parser.add_argument('--n_data', type=int, default=-1)
-parser.add_argument('--npts', type=int, default=10000)
-parser.add_argument('--nrot', type=int, default=10)
-parser.add_argument('--dry-run', action='store_true', default=False,
-                    help='quickly check a single pass')
-args = parser.parse_args()
-print(json.dumps(args.__dict__, indent=2))
+    # Make the planet rotate 10 times over one full orbit
+    time = np.linspace(0, 1, npts)
+    theta = np.linspace(0, 360 * nrot, npts)
 
-if args.dry_run:
-  n_data = 1
-else:
-  n_data = args.n_data #-1 # use all images
-  
-print("computing flux")
-fluxes = make_dataset(args.path_img, n_data, args.ydeg, args.amp, 
-                      args.obl, args.inc, args.npts, args.nrot)
+    # Position of the star relative to the planet in the orbital plane
+    t = np.reshape(time, (1, -1))
+    p = np.vstack((np.cos(2 * np.pi * t), np.sin(2 * np.pi * t), 0 * t))
 
-print("saving...")
-os.makedirs(args.path_save, exist_ok=True)
-with open('{}/configs.json'.format(args.path_save), 'w') as f:
-  json.dump(args.__dict__, f)
-np.save(args.path_save + "/flux", fluxes)
-print("done.")
+    # Rotate to an observer inclination of 60 degrees
+    ci = np.cos(map.inc * np.pi / 180)
+    si = np.sin(map.inc * np.pi / 180)
+    R = np.array([[1, 0, 0], [0, ci, -si], [0, si, ci]])
+    xs, ys, zs = R.dot(p)
+
+    # Keywords to the `flux` method
+    kwargs = dict(theta=theta, xs=xs, ys=ys, zs=zs)
+
+    # Compute the flux
+    flux0 = map.flux(**kwargs)
+    flux = flux0 + sigma * np.random.randn(npts)
+
+    return flux, map
+
+
+def get_device(gpu_id=-1):
+    if gpu_id >= 0 and torch.cuda.is_available():
+        print('device: gpu')
+        return torch.device("cuda", gpu_id), True
+    else:
+        print('device: cpu')
+        return torch.device("cpu"), False
+
+
+class Normalize():
+    def __init__(self, fluxes):
+        self.mean = np.mean(fluxes, axis=0)
+        self.std = np.std(fluxes, axis=0)
+
+    def __call__(self, flux):
+        return np.abs(self.mean - flux) / self.std
+
+
+
+
+
+
+
+
+
+
+
