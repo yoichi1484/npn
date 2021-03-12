df69b95e71c110646275dd7a0e479d72bde3dc63
Yoichi Ishibashi <yoichi1484@gmail.com>
2021-03-12 20:25:37 +0900

    Update make_dataset.py

--- src/make_dataset.py
+++ src/make_dataset.py
@@ -13,8 +13,6 @@ def make_dataset(path_dataset, n_data, ydeg, amp, obl, inc, npts, nrot, fluxes=[
     files = sorted(glob("{}/*.png".format(path_dataset)))
     idx = len(fluxes)
     if n_data > 0:
-        print(len(files), idx, idx+n_data)
-        assert False
         files = files[idx:idx+n_data]
     else:
         files = files[idx:]
